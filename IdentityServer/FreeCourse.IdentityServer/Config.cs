cb86d1a5cf13c9dc75522ded38d761eb402f7018
yasinozlu <ozlulerdenyasin@gmail.com>
2021-06-29 05:00:35 +0300

    Identity Resources Added.

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -9,50 +10,72 @@ namespace FreeCourse.IdentityServer
 {
     public static class Config
     {
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            new ApiResource("resourse_catalog") {Scopes={"catalog_fullpermission"}},
+            new ApiResource("photostock_catalog") {Scopes={"photostock_fullpermission"}},
+            new ApiResource(IdentityServerConstants.LocalApi.ScopeName),
+        };
+
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
+                //new IdentityResources.OpenId(),
+                //new IdentityResources.Profile(),
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
+                new ApiScope("catalog_fullpermission","Catolog API için full erişim"),
+                new ApiScope("photostock_fullpermission","Pohoto Stock API için full erişim"),
+                new ApiScope(IdentityServerConstants.LocalApi.ScopeName),
+
+
+                //new ApiScope("scope1"),
+                //new ApiScope("scope2"),
             };
 
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
-                // m2m client credentials flow client
+
                 new Client
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
+                    ClientName = "Asp.Net Core Mvc",
+                    ClientId = "WebMvcClient",
+                    ClientSecrets = {new Secret("secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+                    AllowedScopes = { "catalog_fullpermission", "photostock_fullpermission",IdentityServerConstants.LocalApi.ScopeName},
+                }
 
-                    AllowedScopes = { "scope1" }
-                },
+                // m2m client credentials flow client
+                //new Client
+                //{
+                //    ClientId = "m2m.client",
+                //    ClientName = "Client Credentials Client",
 
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                //    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
 
-                    AllowedGrantTypes = GrantTypes.Code,
+                //    AllowedScopes = { "scope1" }
+                //},
 
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                //// interactive client using code flow + pkce
+                //new Client
+                //{
+                //    ClientId = "interactive",
+                //    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
 
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
+                //    AllowedGrantTypes = GrantTypes.Code,
+
+                //    RedirectUris = { "https://localhost:44300/signin-oidc" },
+                //    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                //    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                //    AllowOfflineAccess = true,
+                //    AllowedScopes = { "openid", "profile", "scope2" }
+                //},
             };
     }
 }
