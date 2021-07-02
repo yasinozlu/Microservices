9756508116b5848b92126d832aefef8e51330532
yasinozlu <ozlulerdenyasin@gmail.com>
2021-07-03 02:54:42 +0300

    Frontend project has been created.

---
+++ Frontends/FreeCourse.Web/Models/SigninInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Models
+{
+    public class SigninInput
+    {
+        [Display(Name ="Email Adresiniz")]
+        public string Email { get; set; }
+
+        [Display(Name = "Şifreniz")]
+        public string Password { get; set; }
+
+        [Display(Name = "Beni Hatırla")]
+        public bool IsRemember { get; set; }
+    }
+}
