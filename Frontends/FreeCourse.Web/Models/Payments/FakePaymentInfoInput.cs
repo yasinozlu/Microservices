f89d82a103092e7abe9e9ca63481ae1cd3556d69
yasinozlu <ozlulerdenyasin@gmail.com>
2021-07-09 12:21:23 +0300

    Payment Service have been created.

---
+++ Frontends/FreeCourse.Web/Models/Payments/FakePaymentInfoInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Models.Payments
+{
+    public class FakePaymentInfoInput
+    {
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/PaymentService.cs
@@ -0,0 +1,27 @@
+﻿using FreeCourse.Web.Models.Payments;
+using FreeCourse.Web.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PaymentService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> ReceivePayment(FakePaymentInfoInput fakePaymentInfoInput)
+        {
+            var response = await _httpClient.PostAsJsonAsync<FakePaymentInfoInput>("fakepayments", fakePaymentInfoInput);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
