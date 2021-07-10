76490c1417da5ecd0456aa5492f6c5b8014e827f
yasinozlu <ozlulerdenyasin@gmail.com>
2021-07-10 21:45:12 +0300

    Order methods have been created.

---
+++ Frontends/FreeCourse.Web/Models/Orders/OrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Models.Orders
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        //Ödeme geçmişinde adres alanına gerek yok.
+        //public AddressDto Address { get; set; }
+        public string BuyerId { get; set; }
+        public List<OrderItemViewModel> OrderItems { get; set; }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,21 @@
+﻿using FreeCourse.Web.Models.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IOrderService
+    {
+
+        //Senkron iletişim- Order mikroservisine istek gönderikecek.
+        Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput);
+
+
+        //Asenkron iletişim - sipariş bilgileri rabbitMQ ya gönderilecek.
+        Task SuspendOrder(CheckoutInfoInput checkoutInfoInput);
+
+        Task<List<OrderViewModel>> GetOrder();
+    }
+}
